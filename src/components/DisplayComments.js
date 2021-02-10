9552f717c1b73dfcffda4e5ba7d23852d4b7b2ae
Riya <riyanegi221b@gmail.com>
2021-02-10 11:28:54 +0530

    add replies

--- src/components/DisplayComments.js
+++ src/components/DisplayComments.js
@@ -5,32 +5,45 @@ import styles from '../Style.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReply } from '@fortawesome/free-solid-svg-icons'
 
+
+const CommentStructure = ({ i, reply }) => {
+    return (
+        <div className={styles.userInfo} style={reply && { marginLeft: 15 }} >
+            <div>{i.text}</div>
+            <div className={styles.commentsTwo}>
+                <div>
+                    <img
+                        src={i.avatarUrl}
+                        style={{ width: 20, height: 20, borderRadius: 20 / 2 }}
+                        alt='userIcon'
+                    />
+                </div>
+                <div className={styles.fullName}>{i.fullName} </div>
+                <div>
+                    <button className={styles.replyBtn}>
+                        {' '}
+                        <FontAwesomeIcon icon={faReply} size='1x' color='gray' /> Reply
+                     </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const DisplayComments = ({ comments }) => {
     return (
         <div >
             {comments.map((i, index) => (
-                <div className={styles.userInfo} key={i.userId}>
-                    <div>{i.text}</div>
-                    <div className={styles.commentsTwo}>
-                        <div>
-                            <img
-                                src={i.avatarUrl}
-                                style={{ width: 20, height: 20, borderRadius: 20 / 2 }}
-                                alt='userIcon'
-                            />
-                        </div>
-                        <div className={styles.fullName}>{i.fullName} </div>
-                        <div>
-                            <button className={styles.replyBtn}>
-                                {' '}
-                                <FontAwesomeIcon icon={faReply} size='1x' color='gray' /> Reply
-              </button>
-                        </div>
+                <div key={i.userId} >
+                    <CommentStructure i={i} />
+                    <div className={styles.replySection}>
+                        {i.replies && i.replies.map((i, index) => <CommentStructure i={i} key={i.userId} reply />)}
                     </div>
                 </div>
             ))}
         </div>
     )
 }
+
 
 export default DisplayComments
