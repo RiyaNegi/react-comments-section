f552dc3dcd088c60a2d9287060c33fd094b1010e
Riya <riyanegi221b@gmail.com>
2021-02-16 23:42:15 +0530

    add popup lib

--- src/components/DisplayComments.js
+++ src/components/DisplayComments.js
@@ -2,28 +2,52 @@
 import React, { useContext } from 'react'
 import styles from '../Style.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReply } from '@fortawesome/free-solid-svg-icons'
+import { faReply, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import InputField from "./InputField"
 import { ActionContext } from "./ActionContext"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+
 const CommentStructure = ({ i, reply, handleReply }) => {
+    const actions = useContext(ActionContext)
+
     return (
-        <div className={styles.userInfo} style={reply && { marginLeft: 15 }} >
-            <div>{i.text}</div>
-            <div className={styles.commentsTwo}>
-                <div>
-                    <img
-                        src={i.avatarUrl}
-                        style={{ width: 20, height: 20, borderRadius: 20 / 2 }}
-                        alt='userIcon'
-                    />
-                </div>
-                <div className={styles.fullName}>{i.fullName} </div>
-                <div>
-                    <button className={styles.replyBtn} onClick={() => handleReply(i.comId)}>
-                        {' '}
-                        <FontAwesomeIcon icon={faReply} size='1x' color='gray' /> Reply
+        <div className={styles.halfDiv}>
+            <div className={styles.userInfo} style={reply && { marginLeft: 15 }} >
+                <div>{i.text}</div>
+                <div className={styles.commentsTwo}>
+                    <div>
+                        <img
+                            src={i.avatarUrl}
+                            style={{ width: 20, height: 20, borderRadius: 20 / 2 }}
+                            alt='userIcon'
+                        />
+                    </div>
+                    <div className={styles.fullName}>{i.fullName} </div>
+                    <div>
+                        <button className={styles.replyBtn} onClick={() => handleReply(i.comId)}>
+                            {' '}
+                            <FontAwesomeIcon icon={faReply} size='1x' color='#a5a5a5' /> Reply
                      </button>
+                    </div>
                 </div>
+            </div>
+            <div className={styles.userActions}>
+                {actions.userId === i.userId &&
+                    <Popup
+                        role="tooltip"
+                        trigger={<button className={styles.actionsBtn}>
+                            <FontAwesomeIcon icon={faEllipsisV} size='1x' color='#a5a5a5' />
+                        </button>}
+                        position="right center"
+                    >
+                        <ul className={styles.actionsDiv}>
+                            <li> edit</li>
+                            <li> delete</li>
+                        </ul>
+                    </Popup>
+                }
             </div>
         </div>
     )
