96dd5d0f423c5fc5345aa8f9948739e34f554425
Riya <riyanegi221b@gmail.com>
2021-02-24 09:58:40 +0530

    Updates

---
+++ precache-manifest.9cf29e518b71b28c5d4ba2eb965a4c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8ec4e5459be3cb6bd3accae1f4083d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "543f3c23e1f766c9403a",
+    "url": "./static/css/2.9f0d004c.chunk.css"
+  },
+  {
+    "revision": "adf64e5e67f8ce19fbec",
+    "url": "./static/css/main.8535bd8a.chunk.css"
+  },
+  {
+    "revision": "543f3c23e1f766c9403a",
+    "url": "./static/js/2.a543b0e2.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "./static/js/2.a543b0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf64e5e67f8ce19fbec",
+    "url": "./static/js/main.6cb32e80.chunk.js"
+  },
+  {
+    "revision": "5dbb1c10a06729de7d61",
+    "url": "./static/js/runtime-main.98c9bfe7.js"
+  }
+]);
