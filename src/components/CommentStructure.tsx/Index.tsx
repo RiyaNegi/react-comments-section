603c45431de682f7b1e9e03c671c36f775a36cf1
Noor Fatimah <noor.fatimah@infoarc.ai>
2023-04-30 15:06:41 +0500

    Remove change

--- src/components/CommentStructure.tsx/Index.tsx
+++ src/components/CommentStructure.tsx/Index.tsx
@@ -84,8 +84,6 @@ const CommentStructure = ({
           <div className='fullName'>
             {info.fullName} &#09; 
             <span className = "date">
-              {info.updatedAt}
-              {info.createdAt}
               {info.updatedAt ? moment(info.updatedAt).isAfter(info.createdAt) ? moment(info.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
             </span>
           </div>
