32ee13679c36ea12c755786e8276e3dba113e3b0
Noor Fatimah <noor.fatimah@infoarc.ai>
2023-08-20 02:32:46 +0500

    Date Check Edited

--- src/components/CommentStructure.tsx/Index.tsx
+++ src/components/CommentStructure.tsx/Index.tsx
@@ -40,9 +40,8 @@ const CommentStructure = ({
 
   useEffect(() =>
   {
-    console.log(info.updatedAt);
-    console.log(info.createdAt);
-    console.log(info.updatedAt ? moment(info.updatedAt).isAfter(info.createdAt) ? moment(info.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow());
+    console.log(info?.updatedAt);
+    console.log(info?.createdAt);
   }, []);
 
   const optionsMenu = () => {
@@ -91,7 +90,7 @@ const CommentStructure = ({
           <div className='fullName'>
             {info.fullName} &#09; 
             <span className = "date">
-              {(info.updatedAt && moment(info.updatedAt)?.isValid()) ? moment(info.updatedAt).isAfter(info.createdAt) ? moment(info.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
+              {((info?.updatedAt && moment(info?.updatedAt)?.isValid()) && (info?.createdAt && moment(info?.createdAt)?.isValid())) ? moment(info?.updatedAt).isAfter(info?.createdAt) ? moment(info?.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info?.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment().fromNow()}
             </span>
           </div>
         </a>
