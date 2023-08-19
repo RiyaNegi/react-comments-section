c065cdfa19876cc5c87512b2ca43723d9231e2a9
Noor Fatimah <noor.fatimah@infoarc.ai>
2023-08-20 02:01:38 +0500

    Date Check Added

--- src/components/CommentStructure.tsx/Index.tsx
+++ src/components/CommentStructure.tsx/Index.tsx
@@ -91,7 +91,7 @@ const CommentStructure = ({
           <div className='fullName'>
             {info.fullName} &#09; 
             <span className = "date">
-              {info.updatedAt ? moment(info.updatedAt).isAfter(info.createdAt) ? moment(info.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
+              {(info.updatedAt && moment(info.updatedAt)?.isValid()) ? moment(info.updatedAt).isAfter(info.createdAt) ? moment(info.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
             </span>
           </div>
         </a>
