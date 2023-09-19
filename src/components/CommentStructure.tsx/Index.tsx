54188b12973d24487bc617a1c58665a7de01f645
NoorFatimah24 <nfatimah156@gmail.com>
2023-09-19 23:59:10 +0500

    New Update

--- src/components/CommentStructure.tsx/Index.tsx
+++ src/components/CommentStructure.tsx/Index.tsx
@@ -37,7 +37,6 @@ const CommentStructure = ({
 }: CommentStructureProps) => {
   const globalStore: any = useContext(GlobalContext)
   const currentUser = globalStore.currentUserData
-
   const optionsMenu = () => {
     return (
       <div className='userActions'>
@@ -84,7 +83,10 @@ const CommentStructure = ({
           <div className='fullName'>
             {info.fullName} &#09; 
             <span className = "date">
-              {(info?.updatedAt && moment(info?.updatedAt)?.isValid()) ? moment(info?.updatedAt).isAfter(info?.createdAt) ? moment(info?.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' : moment(info?.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow() : moment(info?.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
+              {(info?.updatedAt && moment(info?.updatedAt)?.isValid()) 
+              ? moment(info?.updatedAt, "YYYY-MM-DD HH:mm:ss").fromNow() + ' (edited)' 
+              : (info?.createdAt && moment(info?.createdAt)?.isValid())
+              && moment(info?.createdAt, "YYYY-MM-DD HH:mm:ss").fromNow()}
             </span>
           </div>
         </a>
